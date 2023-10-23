540c31ce578403d6fd3da1fe8225b7fbc4c04f55
hyewon218 <won_11111@naver.com>
2023-10-23 23:53:20 +0900

    Initial commit

---
+++ src/main/java/study/kimquerydsl/KimQuerydslApplication.java
@@ -0,0 +1,13 @@
+package study.kimquerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimQuerydslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KimQuerydslApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/kimquerydsl/KimQuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package study.kimquerydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimQuerydslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
