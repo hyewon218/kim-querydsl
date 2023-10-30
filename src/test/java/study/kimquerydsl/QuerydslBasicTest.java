35a0c8a5bea492df41645f79d172cb5b4b6548d8
hyewon218 <won_11111@naver.com>
2023-10-30 23:18:19 +0900

    동적 쿼리 - BooleanBuilder 사용

--- src/test/java/study/kimquerydsl/QuerydslBasicTest.java
+++ src/test/java/study/kimquerydsl/QuerydslBasicTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.*;
 import static study.kimquerydsl.entity.QMember.*;
 import static study.kimquerydsl.entity.QTeam.*;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
@@ -18,6 +19,7 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.PersistenceUnit;
 import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -621,5 +623,30 @@ public class QuerydslBasicTest {
         for (MemberDto memberDto : result) {
             System.out.println("memberDto = " + memberDto);
         }
+    }
+
+    @Test
+    public void dynamicQuery_BooleanBuilder() {
+        String usernameParam = "member1";
+        Integer ageParam = 10;
+
+        List<Member> result = searchMember1(usernameParam, ageParam);
+
+        Assertions.assertThat(result.size()).isEqualTo(1);
+    }
+
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (usernameCond != null) {
+            builder.and(member.username.eq(usernameCond));
+        }
+        if (ageCond != null) {
+            builder.and(member.age.eq(ageCond));
+        }
+        return queryFactory
+            .selectFrom(member)
+            .where(builder)
+            .fetch();
     }
 }
