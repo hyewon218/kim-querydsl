80a2ebb191377af7c45cbcc9bb41674cb1ec79aa
hyewon218 <won_11111@naver.com>
2023-11-01 00:53:55 +0900

    Spring Data JPA 리포지토리로 변경

---
+++ src/main/java/study/kimquerydsl/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package study.kimquerydsl.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.kimquerydsl.entity.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    // select m from Member m where m.username = ?
+    List<Member> findByUsername(String username);
+}
