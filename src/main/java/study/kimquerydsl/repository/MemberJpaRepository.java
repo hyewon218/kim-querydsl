6ff6465c781af07d12ea8b7e25b7eb0a5cd90dd6
hyewon218 <won_11111@naver.com>
2023-10-31 20:22:43 +0900

    동적 쿼리와 성능 최적화 조회 - Where절 파라미터 사용

--- src/main/java/study/kimquerydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/kimquerydsl/repository/MemberJpaRepository.java
@@ -1,10 +1,12 @@
 package study.kimquerydsl.repository;
 
+import static org.springframework.util.ObjectUtils.isEmpty;
 import static org.springframework.util.StringUtils.hasText;
 import static study.kimquerydsl.entity.QMember.member;
 import static study.kimquerydsl.entity.QTeam.team;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import java.util.List;
@@ -88,6 +90,59 @@ public class MemberJpaRepository {
             .from(member)
             .leftJoin(member.team, team)
             .where(builder)
+            .fetch();
+    }
+
+    //회원명, 팀명, 나이(ageGoe, ageLoe)
+    public List<MemberTeamDto> search(MemberSearchCondition condition) {
+        return queryFactory
+            .select(new QMemberTeamDto(
+                member.id,
+                member.username,
+                member.age,
+                team.id,
+                team.name))
+            .from(member)
+            .leftJoin(member.team, team)
+            .where(usernameEq(condition.getUsername()),
+                teamNameEq(condition.getTeamName()),
+                ageGoe(condition.getAgeGoe()),
+                ageLoe(condition.getAgeLoe())
+                //,ageBetween(condition.getAgeLoe(), condition.getAgeGoe())
+            )
+            .fetch();
+    }
+
+    private BooleanExpression usernameEq(String username) {
+        return isEmpty(username) ? null : member.username.eq(username);
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return isEmpty(teamName) ? null : team.name.eq(teamName);
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe == null ? null : member.age.goe(ageGoe);
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe == null ? null : member.age.loe(ageLoe);
+    }
+
+    // 조합 가능
+    private BooleanExpression ageBetween(int ageLoe, int ageGoe) {
+        return ageLoe(ageLoe).and(ageGoe(ageGoe));
+    }
+
+    // where 파라미터 방식은 이런식으로 재사용 가능
+    public List<Member> findMember(MemberSearchCondition condition) {
+        return queryFactory
+            .selectFrom(member)
+            .leftJoin(member.team, team)
+            .where(usernameEq(condition.getUsername()),
+                teamNameEq(condition.getTeamName()),
+                ageGoe(condition.getAgeGoe()),
+                ageLoe(condition.getAgeLoe()))
             .fetch();
     }
 }
