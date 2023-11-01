42ab488e94afe6e25c3117f2a11059df6fee488f
hyewon218 <won_11111@naver.com>
2023-11-01 14:47:47 +0900

    Spring Data 페이징 활용1 - Querydsl 페이징 연동

--- src/main/java/study/kimquerydsl/repository/MemberRepositoryCustom.java
+++ src/main/java/study/kimquerydsl/repository/MemberRepositoryCustom.java
@@ -1,9 +1,13 @@
 package study.kimquerydsl.repository;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import study.kimquerydsl.dto.MemberSearchCondition;
 import study.kimquerydsl.dto.MemberTeamDto;
 
 public interface MemberRepositoryCustom {
     List<MemberTeamDto> search(MemberSearchCondition condition);
+    Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable);
+    Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable);
 }
